e7e44daf3e2e86edfac3d9b5816a8c2428730a7d
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-11-04 19:11:43 -0400

    comment.test.js more descriptive

--- server/tests/comment.test.js
+++ server/tests/comment.test.js
@@ -2,35 +2,29 @@ const request = require("supertest");
 
 // Test the map routes
 describe("testing the comment routes", () => {
-
   describe("GET /comments", () => {
-    describe("given user is logged in", () => {
-      describe("given map id", () => {
-        it("should return status code 201", async () => {
+    describe("given user is authenticated", () => {
+      test("with a map id, it should return status code 201", async () => {});
 
-        });
-      });
-      describe("given no map id", () => {
-        it("should return status code 201", async () => {});
-      });
+      test("without a map id, it should return status code 201", async () => {});
     });
-    describe("given user is not logged in", () => {
-      it("should return status code 401", async () => {})
+
+    describe("given user is not authenticated", () => {
+      it("should return status code 401", async () => {});
     });
   });
 
   describe("POST /comment", () => {
-    describe("given user is logged in", () => {
-      describe("given a comment", () => {
-        it("should return 201", async () => {});
-      });
-      describe("given no comment", () => {
-        it("should return 400", async () => {});
-      });
+    describe("given user is authenticated", () => {
+      test("with a comment, it should return status code 201", async () => {});
+
+      test("without a comment, it should return status code 400", async () => {});
     });
-    describe("given user is not logged in", () => {
+
+    describe("given user is not authenticated", () => {
       it("should return status code 401", async () => {});
     });
   });
 });
+
 
