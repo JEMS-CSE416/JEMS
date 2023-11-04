107aeb12fc287ebe6a8ebdcb123e3901292bf360
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-11-04 12:22:01 -0400

    jest + super test

---
+++ tests/backend/map.test.js
@@ -0,0 +1,47 @@
+const { describe, default: test } = require("node:test");
+const request = require("supertest");
+
+// Test the map routes
+describe("testing the map routes", () => {
+
+  describe("GET map routes", () => {
+    describe("given user is logged in", () => {
+      describe("given an id of a map", () => {
+        it("should return status code 201", async () => {});
+      });
+      describe("given no id of a map", () => {
+        it("should return status code 201", async () => {});
+      });
+    });
+    describe("given user is not logged in", () => {
+      describe("given an id of a map", () => {});
+      describe("given no id of a map", () => {});
+    });
+  });
+
+  describe("PUT map routes", () => {
+    describe("given user is logged in", () => {
+      describe("given file content of map", () => {
+        it("should return 201", async () => {});
+      });
+      describe("given no file content of a map", () => {
+        it("should return status code 400", async () => {});
+      });
+    });
+    describe("given user is not logged in", () => {});
+  });
+
+  describe("POST map routes", () => {
+    describe("given user is logged in", () => {
+      describe("given a map", () => {
+        it("should return 201", async () => {});
+      });
+      describe("given no map", () => {
+        it("should return 400", async () => {});
+      });
+    });
+    describe("given user is not logged in", () => {
+      it("should return status code 401", async () => {});
+    });
+  });
+});
