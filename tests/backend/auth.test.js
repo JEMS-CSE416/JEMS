d5ee23dacd7a439d5a0dee7cd0518df3566d8bae
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-11-04 14:07:58 -0400

    added auth.test.js

---
+++ tests/backend/auth.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+
+// Test the map routes
+describe("testing the auth routes", () => {
+    
+  describe("GET /loggedIn", () => {});
+
+  describe("POST /logout", () => {});
+
+  describe("POST /login", () => {});
+
+  describe("POST /register", () => {});
+});
