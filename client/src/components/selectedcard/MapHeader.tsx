6bd1739810ac52ecc35405ffb6cdde5190226d80
Sammi Lin <sammi.lin@stonybrook.edu>
2023-11-05 02:19:14 -0500

    mapheader stuffs 2

--- client/src/components/selectedcard/MapHeader.tsx
+++ client/src/components/selectedcard/MapHeader.tsx
@@ -1,11 +1,22 @@
-import "../css/mapHeader.css"
-import { Text } from "@mantine/core";
+import "../css/mapHeader.css";
+import { Text, Group, Image } from "@mantine/core";
+import pencil from "../../assets/images/pencil.png"
 
 const MapHeader = () => {
   return (
     <>
-        <Text fw={700} size="sm" id="creationDate">Created 5 days ago</Text>
-        <Text fw={700} size="lg" id="title">Best Places to Eat in The East Blue</Text>
+      <Text fw={700} size="sm" id="creationDate">
+        Created 5 days ago
+      </Text>
+      <Text fw={700} size="lg" id="title">
+        Best Places to Eat in The East Blue
+      </Text>
+      <Group id="edit">
+        <Image src={pencil} id="editIcon"></Image>
+        <Text size="xs" id="editText">
+          Edit Map
+        </Text>
+      </Group>
     </>
   );
 };
