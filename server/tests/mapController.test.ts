b1ce6f4ff3db624cd84fc28a4acbed99c35efe48
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-11-05 13:08:18 -0500

    create map mongodb test

--- server/tests/mapController.test.ts
+++ server/tests/mapController.test.ts
@@ -1,74 +1,83 @@
 import Fixtures from "node-mongodb-fixtures";
-import mongoose from 'mongoose'
-// import { mapSchema } from './models/Map'
-
-const fixtures = new Fixtures({
-    dir: 'tests/fixtures',
-    mute: false, // do not mute the log output
-  });
-
+import mongoose from "mongoose";
+import { mapSchema } from "./models/Map";
+import map1 from "./models/tests/map1.json";
 
 describe("Testing connection to Mongodb server", () => {
-
-    it("it should exit gracefully", async () => {
-        await fixtures.connect('mongodb://localhost:27017/')
-
-        await mongoose.connect('mongodb://127.0.0.1:27017');
-        
-        await fixtures.disconnect();
+  it("it should exit gracefully", async () => {
+    const fixtures = new Fixtures({
+      dir: "tests/fixtures",
+      mute: false, // do not mute the log output
     });
 
-    test("if unload and load worked by finding a Person", async () => {
-        await fixtures
-        .connect('mongodb://localhost:27017/')
-        .then(() => fixtures.unload())
-        .then(() => fixtures.load())
-        .catch(e => console.error(e))
+    await fixtures.connect("mongodb://localhost:27017/");
 
-        await mongoose.connect('mongodb://127.0.0.1:27017');
-        
-        const personSchema = new mongoose.Schema({
-            name: String,
-            age: Number
-          });
-          
-          
-        const Person = mongoose.model('Person', personSchema);
+    await mongoose.connect("mongodb://127.0.0.1:27017");
 
+    await fixtures.disconnect();
+  });
 
-        const paul = await Person.find({ name: "Paul" });
-
-        console.log(paul[0].name)
-
-        await fixtures.disconnect();
+  test("if unload and load worked by finding a Person", async () => {
+    const fixtures = new Fixtures({
+      dir: "tests/fixtures",
+      mute: false, // do not mute the log output
+      filter: "people.*",
     });
+    await fixtures
+      .connect("mongodb://localhost:27017/")
+      .then(() => fixtures.unload())
+      .then(() => fixtures.load())
+      .catch((e) => console.error(e));
+
+    await mongoose.connect("mongodb://127.0.0.1:27017");
+
+    const personSchema = new mongoose.Schema({
+      name: String,
+      age: Number,
+    });
+
+    const Person = mongoose.model("Person", personSchema);
+
+    const paul = await Person.find({ name: "Paul" });
+
+    console.log(paul[0].name);
+
+    await fixtures.disconnect();
+  });
 });
 
-/*
 describe("Crud Map operations", () => {
-    
-    test("if CREATE a map works", async () => {
-        await fixtures
-        .connect('mongodb://localhost:27017/')
-        .catch(e => console.error(e));
-        
-        await mongoose.connect('mongodb://127.0.0.1:27017');
+  test("if CREATE a map works", async () => {
+    const fixtures = new Fixtures({
+      dir: "tests/fixtures",
+      mute: false, // do not mute the log output
+    });
+    await fixtures.connect("mongodb://localhost:27017/");
 
-        
+    await mongoose.connect("mongodb://127.0.0.1:27017");
 
-    })
+    const Map = mongoose.model("map", mapSchema);
 
-    test("if READ a map works", async () => {
-        
-    })
+    const map = new Map(map1);
+    await map.save();
 
-    test("if UPDATE a map works", async () => {
-        
-    })
+    const retrievedmap = await Map.find({ mapName: "Sample Map" });
 
-    test("if DELETE a map works", async () => {
-        
-    })
+    expect(retrievedmap).toBeDefined();
+    expect(retrievedmap[0].mapName).toBe("Sample Map");
 
-})
-*/
+    await fixtures.disconnect();
+  });
+
+  // test("if READ a map works", async () => {
+
+  // })
+
+  // test("if UPDATE a map works", async () => {
+
+  // })
+
+  // test("if DELETE a map works", async () => {
+
+  // })
+});
