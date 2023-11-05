859829394e78a13517e223c0e7b4be2e5aab44a7
Miguel Maramara <miguel.maramara@stonybrook.edu>
2023-11-04 21:51:47 -0400

    initial endpoint work

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,17 @@
 import express, {Request, Response} from "express"
+import swaggerUi from "swagger-ui-express"
+import * as swaggerDocument from './swagger.json'
 
 const app = express()
 const port = 3000
 
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!!!!')
 })
+
+app.use('/api-docs', swaggerUi.serve)
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
