9ed65c84bb3e7a1dab1281ea2e394bc9b2cd8f19
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-11-11 16:52:11 -0500

    added menu when clicked on profile picture

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -1,5 +1,14 @@
 import "./css/navBar.css";
-import { Menu, Text, rem, Button, Group, Avatar, Image } from "@mantine/core";
+import {
+  Menu,
+  Text,
+  rem,
+  Button,
+  Group,
+  Avatar,
+  Image,
+  Box,
+} from "@mantine/core";
 
 import jemsLogo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
@@ -14,6 +23,9 @@ import {
   IconArrowsLeftRight,
 } from "@tabler/icons-react";
 
+
+
+
 const NavBar = () => {
   const [opened, { open, close }] = useDisclosure(false);
   return (
@@ -27,78 +39,76 @@ const NavBar = () => {
         <Button radius="xl" id="createMapButton" onClick={open}>
           + Create Map
         </Button>
-        <Menu styles={{
-          root:
-        }}>
-          <Menu.Target>
-            <Avatar color="blue" radius="xl" id="profilePhoto">
-              M
-            </Avatar>
-          </Menu.Target>
 
-          <Menu.Dropdown>
-            <Menu.Label>Application</Menu.Label>
-            <Menu.Item
-              leftSection={
-                <IconSettings style={{ width: rem(14), height: rem(14) }} />
-              }
-            >
-              Settings
-            </Menu.Item>
-            <Menu.Item
-              leftSection={
-                <IconMessageCircle
-                  style={{ width: rem(14), height: rem(14) }}
-                />
-              }
-            >
-              Messages
-            </Menu.Item>
-            <Menu.Item
-              leftSection={
-                <IconPhoto style={{ width: rem(14), height: rem(14) }} />
-              }
-            >
-              Gallery
-            </Menu.Item>
-            <Menu.Item
-              leftSection={
-                <IconSearch style={{ width: rem(14), height: rem(14) }} />
-              }
-              rightSection={
-                <Text size="xs" c="dimmed">
-                  ⌘K
-                </Text>
-              }
-            >
-              Search
-            </Menu.Item>
+        <Box id="user-profile">
+          <Menu>
+            <Menu.Target>
+              <Avatar color="blue" radius="xl" className="cursor-pointer">
+                M
+              </Avatar>
+            </Menu.Target>
 
-            <Menu.Divider />
+            <Menu.Dropdown>
+              <Menu.Label>Application</Menu.Label>
+              <Menu.Item
+                leftSection={
+                  <IconSettings style={{ width: rem(14), height: rem(14) }} />
+                }
+              >
+                Settings
+              </Menu.Item>
+              <Menu.Item
+                leftSection={
+                  <IconMessageCircle
+                    style={{ width: rem(14), height: rem(14) }}
+                  />
+                }
+              >
+                Messages
+              </Menu.Item>
+              <Menu.Item
+                leftSection={
+                  <IconPhoto style={{ width: rem(14), height: rem(14) }} />
+                }
+              >
+                Gallery
+              </Menu.Item>
+              <Menu.Item
+                leftSection={
+                  <IconSearch style={{ width: rem(14), height: rem(14) }} />
+                }
+                rightSection={
+                  <Text size="xs" c="dimmed">
+                    ⌘K
+                  </Text>
+                }
+              >
+                Search
+              </Menu.Item>
 
-            <Menu.Label>Danger zone</Menu.Label>
-            <Menu.Item
-              leftSection={
-                <IconArrowsLeftRight
-                  style={{ width: rem(14), height: rem(14) }}
-                />
-              }
-            >
-              Transfer my data
-            </Menu.Item>
-            <Menu.Item
-              color="red"
-              leftSection={
-                <IconTrash style={{ width: rem(14), height: rem(14) }} />
-              }
-            >
-              Delete my account
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-        {/* <Avatar color="blue" radius="xl" id="profilePhoto">
-          L
-        </Avatar> */}
+              <Menu.Divider />
+
+              <Menu.Label>Danger zone</Menu.Label>
+              <Menu.Item
+                leftSection={
+                  <IconArrowsLeftRight
+                    style={{ width: rem(14), height: rem(14) }}
+                  />
+                }
+              >
+                Transfer my data
+              </Menu.Item>
+              <Menu.Item
+                color="red"
+                leftSection={
+                  <IconTrash style={{ width: rem(14), height: rem(14) }} />
+                }
+              >
+                Delete my account
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </Box>
       </Group>
     </div>
   );
