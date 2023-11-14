67eee3d7d6695fe34c57d5c188c39058d13009a2
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-11-14 00:45:36 -0500

    Refactor map display on HomeScreen component

--- client/src/components/browsing/MapCard.tsx
+++ client/src/components/browsing/MapCard.tsx
@@ -11,7 +11,7 @@ type MapCardProps = {
 
 const MapCard: React.FC<MapCardProps> = ({ name, isPrivate }) => {
   return (
-    <Card shadow="sm" padding="xl" radius="md" withBorder id="card">
+    <Card shadow="sm" padding="xl" radius="md" withBorder className="card">
       <Card.Section>
         <Image
           src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
@@ -96,7 +96,5 @@ const MapCard: React.FC<MapCardProps> = ({ name, isPrivate }) => {
     </Card>
   );
 };
-function randomTest() {
-  console.log("lool");
-}
+
 export default MapCard;
