8a1f25bd635f34af262cc2f075315f4a48020fe2
Miguel Maramara <miguel.maramara@stonybrook.edu>
2023-11-15 04:09:28 -0500

    Basic Appshell

--- client/src/components/edit/Edit.tsx
+++ client/src/components/edit/Edit.tsx
@@ -1,3 +1,4 @@
+import { AppShell } from "@mantine/core";
 import { useParams } from "react-router-dom";
 import { EditContextProvider } from '../../context/EditContextProvider';
 import Canvas from "./Canvas";
@@ -6,11 +7,36 @@ import EditNavBar from "./EditNavbar";
 export default function Edit(){
   const { id } = useParams();
   return (
-      <EditContextProvider map_id={id}>
-        <EditNavBar/>
-        <Canvas>
-        </Canvas>
-      </EditContextProvider>
+    <EditContextProvider map_id={id}>
+      <AppShell
+        header={{height: 60, offset:true}}
+      >
+        {/* Top element */}
+        <AppShell.Header>
+          <EditNavBar/>
+        </AppShell.Header>
+
+        {/* Left element */}
+        <AppShell.Navbar zIndex={-1}>
+          <h1>
+            Test
+          </h1>
+        </AppShell.Navbar>
+
+        {/* Middle element */}
+        <AppShell.Main>
+          <Canvas>
+          </Canvas>
+        </AppShell.Main>
+
+        {/* Right element */}
+        <AppShell.Aside zIndex={-1}>
+          <h1>
+            Test
+          </h1>
+        </AppShell.Aside>
+      </AppShell>
+    </EditContextProvider>
   );
 }
 
