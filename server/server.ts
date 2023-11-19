a0ce69dba72ab5dcfd0451d6f2dcea9f5eb8a58d
Miguel Maramara <41304780+miguelcmaramara@users.noreply.github.com>
2023-11-19 01:11:08 -0500

    changing port

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from "express"
 import app from './app'
 
 
-const port = 3000
+const port = 443
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
