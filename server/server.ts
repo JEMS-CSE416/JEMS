542a3bb908849b9cadf93084e8b26607ee88c1b6
Miguel Maramara <41304780+miguelcmaramara@users.noreply.github.com>
2023-11-19 01:30:05 -0500

    added certificate

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,18 @@
 import express, {Request, Response} from "express"
 import app from './app'
+import * as fs from 'fs';
 
 
-const port = 80
+const port = 443
+var privateKey = fs.readFileSync( '/etc/letsencrypt/live/dev-jems-api.miguelmaramara.com/privkey.pem' );
+var certificate = fs.readFileSync( '/etc/letsencrypt/live/dev-jems-api.miguelmaramara.com/fullchain.pem' );
 
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(port);
+
+https.createServer
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
