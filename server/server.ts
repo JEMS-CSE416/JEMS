2216bd77ec472123a8eded3165225f6e229622e4
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-11-19 16:41:34 -0500

    Refactor server.ts to support HTTPS

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,29 @@
-import express, {Request, Response} from "express"
-import app from './app'
-import * as fs from 'fs';
-import * as https from 'https'
+import express, { Request, Response } from "express";
+import app from "./app";
+import * as fs from "fs";
+import * as https from "https";
 
+const port = 443;
+try {
+  var privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/dev-jems-api.miguelmaramara.com/privkey.pem"
+  );
+  var certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/dev-jems-api.miguelmaramara.com/fullchain.pem"
+  );
 
-const port = 443
-var privateKey = fs.readFileSync( '/etc/letsencrypt/live/dev-jems-api.miguelmaramara.com/privkey.pem' );
-var certificate = fs.readFileSync( '/etc/letsencrypt/live/dev-jems-api.miguelmaramara.com/fullchain.pem' );
-
-https.createServer({
-    key: privateKey,
-    cert: certificate
-}, app).listen(port);
+  https
+    .createServer(
+      {
+        key: privateKey,
+        cert: certificate,
+      },
+      app
+    )
+    .listen(port);
+  console.log(`HTTPS Server listening on port ${port}`);
+} catch {
+  app.listen(port, () => {
+    console.log(`HTTP Server listening on port ${port}`);
+  });
+}
