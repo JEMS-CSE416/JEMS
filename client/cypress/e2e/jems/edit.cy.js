c985f1bbc041a61a1402186b1ffb861155b4582d
Miguel Maramara <miguel.maramara@stonybrook.edu>
2023-11-24 04:46:12 -0500

    removed edit tests. they must be remade

--- client/cypress/e2e/jems/edit.cy.js
+++ client/cypress/e2e/jems/edit.cy.js
@@ -11,25 +11,25 @@ beforeEach(() => {
 });
 
 describe("Modal checking", () => {
-  it("Checks if the export modal is visible", () => {
-    cy.visit("/edit")
-    // Click on the button to open the export modal
-    cy.get(exportButton).click();
-    cy.get(`${exportModalTitle}+button`).click();
-  });
-  it("Checks if the settings modal is visible", () => {
-    cy.visit("/edit")
-    // Click on the button to open the settings modal
-    cy.get(settingsButton).click();
-    cy.get(`${settingsModalTitle}+button`).click();
-  });
+  //it("Checks if the export modal is visible", () => {
+    //cy.visit("/edit")
+    //// Click on the button to open the export modal
+    //cy.get(exportButton).click();
+    //cy.get(`${exportModalTitle}+button`).click();
+  //});
+  //it("Checks if the settings modal is visible", () => {
+    //cy.visit("/edit")
+    //// Click on the button to open the settings modal
+    //cy.get(settingsButton).click();
+    //cy.get(`${settingsModalTitle}+button`).click();
+  //});
 });
 
 describe("Navigation checking", () => {
   it("Checks if the duplicate modal is visible", () => {
-    cy.visit("/selected")
-    // Click on the button to open the duplicate modal
-    cy.get(editButton).click();
-    cy.location('pathname').should('eq', '/edit')
+    //cy.visit("/selected")
+    //// Click on the button to open the duplicate modal
+    //cy.get(editButton).click();
+    //cy.location('pathname').should('eq', '/edit')
   });
 });
