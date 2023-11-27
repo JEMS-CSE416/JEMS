651e7f2e49c77242e4ac6cd205273bede8bcc3bd
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-11-27 15:07:26 -0500

    editing

--- client/src/components/edit/Properties.tsx
+++ client/src/components/edit/Properties.tsx
@@ -12,6 +12,7 @@ import {
   NumberInput,
   SegmentedControl,
   Paper,
+  Button,
 } from "@mantine/core";
 import {
   EditPageAction,
@@ -25,13 +26,56 @@ import { useEffect, useState } from "react";
 export default function Properties() {
   const editPageState = useEditContext();
   const setEditPageState = useEditDispatchContext();
+
   const [groupNameState, setGroupNameState] = useState(
     editPageState.selectedRegion?.groupName
   );
 
+  const [regionNameState, setRegionNameState] = useState(
+    editPageState.selectedRegion?.region.regionName
+  );
+
+  const [stringLabelState, setStringLabelState] = useState(
+    editPageState.selectedRegion?.region.stringLabel
+  );
+
+  const [numericLabelState, setNumericLabelState] = useState<string|number>(editPageState.selectedRegion?.region.numericLabel.toString() ?? "");
+
+  const [numericUnitsState, setNumericUnitsState] = useState(
+    editPageState.selectedRegion?.region.numericUnit
+  );
+
+  const [colorState, setColorState] = useState(
+    editPageState.selectedRegion?.region.color
+  );
+
   useEffect(() => {
     setGroupNameState(editPageState.selectedRegion?.groupName);
+    setRegionNameState(editPageState.selectedRegion?.region.regionName);
+    setStringLabelState(editPageState.selectedRegion?.region.stringLabel);
+    setNumericLabelState(editPageState.selectedRegion?.region.numericLabel.toString() ?? "");
+    setNumericUnitsState(editPageState.selectedRegion?.region.numericUnit);
+    setColorState(editPageState.selectedRegion?.region.color);
   }, [editPageState.selectedRegion]);
+
+  const handleRegionPropertyEditing = () => {
+    console.log(
+      "locking in group name from ",
+      editPageState.selectedRegion!.groupName,
+      "to ",
+      groupNameState
+    );
+    setEditPageState({
+      type: "update_selected_region_group_name",
+      selectedRegion: {
+        ...editPageState.selectedRegion!,
+        groupName:
+          groupNameState === ""
+            ? editPageState.selectedRegion!.groupName
+            : groupNameState!,
+      },
+    });
+  };
 
   return (
     <Box
@@ -111,11 +155,13 @@ export default function Properties() {
               Editing: {editPageState.selectedRegion?.region.regionName}
             </Title>
             <Title order={6}>
-              Group Name ({editPageState.selectedRegion?.i}): {editPageState.selectedRegion?.groupName}
+              Group Name ({editPageState.selectedRegion?.i}):{" "}
+              {editPageState.selectedRegion?.groupName}
             </Title>
             <Stack pl={10} gap="xs" p="sm">
               <Title order={6}> Region Properties </Title>
               <Center></Center>
+
               <TextInput
                 label="Group Name"
                 placeholder="Group Name"
@@ -123,37 +169,69 @@ export default function Properties() {
                 onChange={(event) => {
                   setGroupNameState(event.currentTarget.value);
                 }}
-                onKeyDown={(event) => {
-                  if (event.key === "Enter") {
-                    console.log(
-                      "locking in group name from ",
-                      editPageState.selectedRegion!.groupName,
-                      "to ",
-                      groupNameState
-                    );
-                    setEditPageState({
-                      type: "update_selected_region_group_name",
-                      selectedRegion: {
-                        ...editPageState.selectedRegion!,
-                        groupName:
-                          groupNameState === ""
-                            ? editPageState.selectedRegion!.groupName
-                            : groupNameState!,
-                      },
-                    });
-                  }
+              />
+
+              <TextInput
+                label="Region Name"
+                placeholder="Region Name"
+                value={regionNameState}
+                onChange={(event) => {
+                  setRegionNameState(event.currentTarget.value);
                 }}
               />
 
-              <TextInput label="Region Name" placeholder="Region Name" />
+              <TextInput
+                label="String Label"
+                placeholder="String Label"
+                value={stringLabelState}
+                onChange={(event) => {
+                  setStringLabelState(event.currentTarget.value);
+                }}
+              />
 
-              <TextInput label="String Label" placeholder="String Label" />
+              <NumberInput
+                hideControls
+                label="Numeric Label"
+                placeholder="100, 250, etc."
+                value={numericLabelState}
+                onChange={setNumericLabelState}
+              />
+              {/* <TextInput
+                label="Numeric Label"
+                placeholder="Numeric Label"
+                value={numericLabelState?.toString()}
+                onChange={(event) => {
+                  const value = parseFloat(event.currentTarget.value);
+                  if (!isNaN(value)) {
+                    setNumericLabelState(value);
+                  }
+                }}
+              /> */}
 
-              <TextInput label="Numeric Label" placeholder="Numeric Label" />
+              <TextInput
+                label="Numeric Units"
+                placeholder="100K, 2M, etc."
+                value={numericUnitsState}
+                onChange={(event) =>
+                  setNumericUnitsState(event.currentTarget.value)
+                }
+              />
 
-              <NumberInput label="Numeric Units" placeholder="Numeric Units" />
+              {/* <NumberInput
+                label="Numeric Units"
+                placeholder="Numeric Units"
+                value={numericUnitsState}
+                onChange={setNumericUnitsState}
+              /> */}
 
-              <ColorInput label="Input label" placeholder="#000000" />
+              <ColorInput
+                label="Color"
+                placeholder="#000000"
+                value={colorState}
+                onChange={setColorState}
+              />
+
+              <Button radius="xl">Done</Button>
             </Stack>
           </>
         )}
--- client/src/context/EditContextProvider.tsx
+++ client/src/context/EditContextProvider.tsx
@@ -30,9 +30,9 @@ export interface EditPageState {
 }
 
 export interface EditPageAction {
-  type: String,
-  map?: Map,
-  modal?: String,
+  type: String;
+  map?: Map;
+  modal?: String;
   selectedRegion?: {
     groupName: string;
     i: number;
@@ -70,11 +70,10 @@ export function EditContextProvider(props: EditContextProviderProps) {
     fetchMap();
   }, []);
 
-
   const fetchMap = async () => {
     // Replace with your API endpoint and map ID
-    const apiUrl = BACKEND_URL+"/api/maps/{id}/?id=";
-    const mapId = props.map_id; 
+    const apiUrl = BACKEND_URL + "/api/maps/{id}/?id=";
+    const mapId = props.map_id;
 
     try {
       console.log(`fetching map for id: ${mapId}`);
@@ -117,16 +116,18 @@ function editReducer(state: EditPageState, action: any): EditPageState {
     case "change_modal":
       return {
         ...state,
-        modal: Object.values(EditModalEnum).includes(action.modal) ? action.modal : "NONE"
-      }
-    case 'update_map':
-      console.log("updating map state")
-      console.log(action.map)
+        modal: Object.values(EditModalEnum).includes(action.modal)
+          ? action.modal
+          : "NONE",
+      };
+    case "update_map":
+      console.log("updating map state");
+      console.log(action.map);
       return {
         ...state,
-        map: action.map ?? ErrorMap
-      }
-    case 'select_region':
+        map: action.map ?? ErrorMap,
+      };
+    case "select_region":
       const selectedRegion = {
         i: action.selectedRegion.i,
         groupName: action.selectedRegion.groupName,
@@ -152,28 +153,55 @@ function editReducer(state: EditPageState, action: any): EditPageState {
     case "update_selected_region_group_name":
       const oldGroupName = state.selectedRegion!.groupName;
       const newGroupName = action.selectedRegion.groupName;
-      const regions = state.map.regions;
-      if (newGroupName in state.map.regions) {
+      const newRegions = { ...state.map.regions };
+      if (newGroupName in newRegions) {
         // If the groupName exists
+        console.log("adding to existing group: ", newGroupName);
         //update the corresponding region to have the region.
+        newRegions[newGroupName] = [
+          ...newRegions[newGroupName],
+          action.selectedRegion.region,
+        ];
         // Then remove the old region from the old group.
+        newRegions[oldGroupName] = newRegions[oldGroupName].filter(
+          (region) =>
+            region.regionName !== action.selectedRegion.region.regionName
+        );
         // If the old group is now empty then remove it.
+        if (newRegions[oldGroupName].length === 0) {
+          delete newRegions[oldGroupName];
+        }
       } else {
         // If the groupName doesn't exist
         console.log("adding new group: ", newGroupName);
         // add a new group to the regions map and add the region to the group.
-        regions[newGroupName] = [action.selectedRegion.region];
+        newRegions[newGroupName] = [action.selectedRegion.region];
         // then remove the region from the old group.
-        regions[oldGroupName] = regions[oldGroupName].filter(region => region.regionName !== action.selectedRegion.region.regionName);
+        newRegions[oldGroupName] = newRegions[oldGroupName].filter(
+          (region) =>
+            region.regionName !== action.selectedRegion.region.regionName
+        );
         // If the old group is now empty then remove it.
-        if (regions[oldGroupName].length === 0) {
-          delete regions[oldGroupName];
+        if (newRegions[oldGroupName].length === 0) {
+          delete newRegions[oldGroupName];
         }
       }
 
-      console.log("updated state: ", state);
+      console.log("updated state: ", {
+        ...state,
+        map: {
+          ...state.map,
+          regions: newRegions,
+        },
+      });
+
+      
       return {
         ...state,
+        map: {
+          ...state.map,
+          regions: newRegions,
+        },
       };
     case "update_map":
       return {
@@ -181,7 +209,6 @@ function editReducer(state: EditPageState, action: any): EditPageState {
         map: action.map,
       };
   }
-
   return state;
 }
 
