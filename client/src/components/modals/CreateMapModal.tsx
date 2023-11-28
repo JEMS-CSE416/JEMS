0903cd84ee47512d5939d7dab4223a6d67252e9b
jendyren <jendyren@gmail.com>
2023-11-28 16:21:23 -0500

    change map card image based on url

--- client/src/components/modals/CreateMapModal.tsx
+++ client/src/components/modals/CreateMapModal.tsx
@@ -161,8 +161,8 @@ const CreateMapModalBase: React.FC<CreateMapModalProps> = ({ opened, onClose }) 
           displayLegend: false,
           displayPointers: false,
           thumbnail: {
-            imageUrl: "",
-            imageType: "",
+            imageUrl: "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80",
+            imageType: "avif",
           },
           regions: {
             [filename]: getRegions(geojson),
