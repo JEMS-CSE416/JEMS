97317bcab2a59631b8124a4df13d05c161baff25
Sammi Lin <64671027+sammi-lin1@users.noreply.github.com>
2023-11-29 01:22:37 -0500

    save map (#94)
    * save map
    
    * alert added

--- client/src/components/edit/EditNavbar.tsx
+++ client/src/components/edit/EditNavbar.tsx
@@ -1,71 +1,96 @@
+import { Button, Flex, Text } from "@mantine/core";
 import {
-  Button,
-  Flex,
-  Text,
-} from "@mantine/core";
-import { useEditContext, useEditDispatchContext } from "../../context/EditContextProvider";
+  useEditContext,
+  useEditDispatchContext,
+} from "../../context/EditContextProvider";
 import { BaseNavbar } from "../common/Navbar";
 import {
   IconDeviceFloppy,
   IconShare2,
-  IconSettings
+  IconSettings,
 } from "@tabler/icons-react";
 import { ExportMapModal } from "../modals/ExportMapModal";
 import { SettingsMapModal } from "../modals/SettingsMapModal";
-
+import { updateMap } from "../../api/MapApiAccessor";
+import { notifications } from "@mantine/notifications";
+import { IconCheck, IconX } from "@tabler/icons-react";
 
 /*
  * Edit navbar
  */
-export default function EditNavBar(){
+export default function EditNavBar() {
   const editState = useEditContext();
 
   return (
     <BaseNavbar
-      modals= {
+      modals={
         <>
-          <ExportMapModal/>
-          <SettingsMapModal/>
+          <ExportMapModal />
+          <SettingsMapModal />
         </>
       }
-      center_component={
-        <MapTitle name={editState.map.mapName}/>
-      }
-      right_component={ <EditNavButtons/> }
+      center_component={<MapTitle name={editState.map.mapName} />}
+      right_component={<EditNavButtons />}
     />
-  )
-};
-
-function MapTitle(props: {name: string}){
-  return(
-    <Text fw={700} size="xl" id="homePageHeader" >
-      {props.name}
-    </Text>
-  )
+  );
 }
 
-function EditNavButtons(){
-  const setEditPageState = useEditDispatchContext() ;
+function MapTitle(props: { name: string }) {
+  return (
+    <Text fw={700} size="xl" id="homePageHeader">
+      {props.name}
+    </Text>
+  );
+}
+
+function EditNavButtons() {
+  const editState = useEditContext();
+  async function CallUpdateMap() {
+    try {
+      const map = await updateMap({ map: editState.map });
+
+      // Show a notification
+      notifications.show({
+        icon: <IconCheck />,
+        title: "Your map has been saved!",
+        message: "A map never to be lost again!",
+      });
+    } catch (error) {
+      console.log(error);
+      // Show a notification
+      notifications.show({
+        icon: <IconX />,
+        title: "Error updating map",
+        message: "Please try again",
+      });
+    }
+  }
+  const setEditPageState = useEditDispatchContext();
   const iconSize = 20;
 
   return (
-    <Flex
-      gap="sm"
-      justify="center"
-    >
-      <Button radius="xl" >
-        <IconDeviceFloppy size={iconSize}/>
+    <Flex gap="sm" justify="center">
+      <Button radius="xl" onClick={CallUpdateMap}>
+        <IconDeviceFloppy size={iconSize} />
       </Button>
-      <Button id="export-button" radius="xl" onClick={
-          () => setEditPageState({ type:"change_modal", modal:"MAP_EXPORT" })
-        }>
-        <IconShare2 size={iconSize}/>
+      <Button
+        id="export-button"
+        radius="xl"
+        onClick={() =>
+          setEditPageState({ type: "change_modal", modal: "MAP_EXPORT" })
+        }
+      >
+        <IconShare2 size={iconSize} />
       </Button>
-      <Button id="settings-button" radius="xl" onClick={
-          () => setEditPageState({ type:"change_modal", modal:"MAP_SETTINGS" })
-        }>
-        <IconSettings size={iconSize}/>
+      <Button
+        id="settings-button"
+        radius="xl"
+        onClick={() =>
+          setEditPageState({ type: "change_modal", modal: "MAP_SETTINGS" })
+        }
+      >
+        <IconSettings size={iconSize} />
       </Button>
     </Flex>
-  )
+  );
 }
