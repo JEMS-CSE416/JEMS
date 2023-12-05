e666310ca610327e20c23856065429bc88d145c1
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-12-04 20:02:34 -0500

    Add User model to MapHeader component

--- client/src/components/selectedcard/SelectedCardPage.tsx
+++ client/src/components/selectedcard/SelectedCardPage.tsx
@@ -8,33 +8,57 @@ import Footer from "../common/Footer";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { Map } from "../../utils/models/Map";
+import { User } from "../../utils/models/User";
 import { useAuth } from "../hooks/useAuth";
 import { useLoadingData } from "../hooks/useLoadingData";
 import { getMap } from "../../api/MapApiAccessor";
-
+import { useEffect, useState } from "react";
+import { getUser } from "../../api/UserApiAccesor";
 
 const SelectedCardPage = ({}) => {
   const { id } = useParams();
   const { isLoggedIn, user } = useAuth();
   const navigate = useNavigate();
-  const { data: map, loading, error } = useLoadingData(getMap, [{ id }]);
+  const {
+    data: map,
+    loading: mapLoading,
+    error: mapError,
+  } = useLoadingData<Map>(getMap, [{ id }]);
+  const [mapUser, setMapUser] = useState<User>(); 
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (map) {
+        try {
+          const user = await getUser(map.creatorId);
+          setMapUser(user);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+
+    fetchUser();
+  }, [map]);
 
   if (!isLoggedIn) {
     navigate("/login");
   }
+
   return (
     <>
-      {loading ? (
+      {mapLoading ? (
         <div>Loading...</div>
       ) : (
-        map && (
+        map &&
+        mapUser && (
           <>
             <NavBar />
             <div id="content">
-              <MapHeader map={map}/>
-              <Canvas map={map}/>
-              <MapAbout map={map}/>
-              <Comments/>
+              <MapHeader map={map} mapUser={mapUser} />
+              <Canvas map={map} />
+              <MapAbout map={map} />
+              <Comments />
             </div>
             <Footer />
           </>
