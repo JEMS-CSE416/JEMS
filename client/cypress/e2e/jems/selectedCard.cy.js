15c709c0cdaac14d491d37593c01ad88a80d89da
jendyren <jendyren@gmail.com>
2023-12-18 13:04:55 -0500

    create map test

--- client/cypress/e2e/jems/selectedCard.cy.js
+++ client/cypress/e2e/jems/selectedCard.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-import { splashGetStartedButton } from "./id";
+import { login } from "./utils";
 const duplicateButton = "#duplicate-button";
 const duplicateModal = "#duplicate-modal";
 const duplicateModalSubmitButton = "#duplicate-modal-submit-button";
@@ -11,33 +11,12 @@ const deleteButton = "#delete-button";
 const deleteModalConfirmButton = "#delete-modal-confirm-button";
 const deleteModalCancelButton = "#delete-modal-cancel-button";
 
-const loginEmailInput = "#loginEmailInput";
-const loginPasswordInput = "#loginPasswordInput";
-const loginSubmitButton = "#loginButton";
-
-function login() {
-  cy.visit("/");
-
-  // click on the get started button
-  cy.get(splashGetStartedButton).click();
-
-  cy.get(loginEmailInput).type("jendyren@gmail.com");
-  cy.get(loginPasswordInput).type("12345678");
-
-  // clicks on the login button
-  cy.get(loginSubmitButton).click();
-
-  // checks if the url includes /home
-  cy.url().should("include", "/home");
-}
-
 beforeEach(() => {
-  // Login 
+  // Login
   login();
-  // click on card id of 65666368ac743f6444ab4773
-  cy.get("#6570831f904450313965d9a5").click();
+  // click on the first card in the home page
+  cy.get(".card.cursor-pointer").first().click();
 });
-
 
 describe("Modal checking", () => {
   it("Checks if the duplicate modal is visible", () => {
@@ -50,5 +29,4 @@ describe("Modal checking", () => {
     // Click on the button to open the duplicate modal
     cy.get(downloadButton).click();
   });
-
 });
